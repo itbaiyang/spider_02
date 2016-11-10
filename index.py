361f5a3d642a78e335a9428cef7f08501901ee9b
zrh <459676797@qq.com>
2016-11-10 14:59:12 +0800

    afda

---
+++ index.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+import requests
+import random
+import time
+import socket
+import http.client
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+import logging
+# from log import init_logging
+client = MongoClient('127.0.0.1', 27017)
+db = client.spider
+collection = db.shenhua
+print(collection.count())
+
+
+def get_content(url, data = None):
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116'
+    }
+    timeout = random.choice(range(80, 180))
+    while True:
+        try:
+            rep = requests.get(url, headers=header, timeout=timeout)
+            rep.encoding = 'gbk'
+            break
+        except socket.timeout as e:
+            print('3:', e)
+            time.sleep(random.choice(range(8, 15)))
+        except socket.error as e:
+            print('4:', e)
+            time.sleep(random.choice(range(20, 60)))
+
+        except http.client.BadStatusLine as e:
+            print('5:', e)
+            time.sleep(random.choice(range(30, 80)))
+
+        except http.client.IncompleteRead as e:
+            print('6:', e)
+            time.sleep(random.choice(range(5, 15)))
+    return rep.text
+
+
+def get_content_detail(url, data = None):
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116'
+    }
+    timeout = random.choice(range(80, 180))
+    while True:
+        try:
+            rep = requests.get(url, headers=header, timeout=timeout)
+            rep.encoding = 'utf-8'
+            break
+        except socket.timeout as e:
+            print('3:', e)
+            time.sleep(random.choice(range(8, 15)))
+        except socket.error as e:
+            print('4:', e)
+            time.sleep(random.choice(range(20, 60)))
+
+        except http.client.BadStatusLine as e:
+            print('5:', e)
+            time.sleep(random.choice(range(30, 80)))
+
+        except http.client.IncompleteRead as e:
+            print('6:', e)
+            time.sleep(random.choice(range(5, 15)))
+    return rep.text
+
+
+def get_data(html_text):
+    final = []
+    bs = BeautifulSoup(html_text, "html.parser")  # 创建BeautifulSoup对象
+    data = bs.find_all(class_='r-block')
+
+    for day in data:
+        bid_sh = {}
+        pass
+        bid_sh['link'] = day.find('a').get('href')
+        # bid_sh['name'] = day.find('a').get('title')
+        bid_sh['time'] = day.find('span').string
+        # print(bid_sh)
+        bid_html = get_content_detail('http://www.shenhuabidding.com.cn'+bid_sh['link'])
+        soup = BeautifulSoup(bid_html, "html.parser")
+        tr = soup.find_all('tr')
+        td = tr[1].find_all('td')
+        bid_sh['bid_num'] = td[0].string
+        bid_sh['name'] = td[1].string
+        bid_sh['company_name'] = td[2].string
+        print(bid_sh['bid_num'])
+        collection.insert(bid_sh)
+    return final
+
+
+if __name__ == '__main__':
+    # init_logging()
+    for i in range(1, 13):
+        url = 'http://www.shenhuabidding.com.cn/bidweb/001/001006/'+str(i)+'.html'
+        html = get_content(url)
+        result = get_data(html)
+
